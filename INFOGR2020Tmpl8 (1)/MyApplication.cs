e473df02752b331de9300a419a6bf6564dae616a
Irshad <irsahd.n.b@hotmail.com>
2020-05-20 14:35:39 +0200

    Working

--- INFOGR2020Tmpl8 (1)/MyApplication.cs
+++ INFOGR2020Tmpl8 (1)/MyApplication.cs
@@ -19,11 +19,16 @@ namespace Template
 		// initialize
 		public void Init()
 		{
+
+
 			light = new List<Circle>();
 			primitives = new List<Circle>();
 
-
-			light.Add(new Circle(0, 0, 0.1f, true, new floatColour(1, 1, 1)));
+			primitives.Add(new Circle(0.3f, 0.1f, 0.1f, false, new floatColour(0, 0, 0)));
+			primitives.Add(new Circle(-0.5f, -0.5f, 0.1f, false, new floatColour(1, 1, 1)));
+			light.Add(new Circle(0f, 0.5f, 0.25f, true, new floatColour(1, 0, 1)));
+			light.Add(new Circle(-0.3f, 0.2f, 0.25f, true, new floatColour(0, 1, 0)));
+			light.Add(new Circle(0.2f, -0.4f, 0.25f, true, new floatColour(0, 0, 1)));
 
 			ray = new Ray();
 			for(int x = 0; x < 640; x++)
@@ -33,51 +38,54 @@ namespace Template
 					floatColour pixelColour = new floatColour(0, 0, 0);
 					foreach(Circle c in light)
 					{
-
 						ray.O = pixelPosition(x, y);
-						ray.D = (new Vector2(c.x - ray.O.X, c.y - ray.O.Y)).Normalized();
-						ray.t = (float)Math.Sqrt(Math.Pow(c.x - ray.O.X, 2) + Math.Pow(c.y - ray.O.Y, 2));
+						ray.D = (new Vector2(ray.O.X - c.x, ray.O.Y - c.y)).Normalized();
+						ray.t = (float)Math.Sqrt(Math.Pow(ray.O.X - c.x, 2) + Math.Pow(ray.O.Y - c.y, 2));
 
 						bool occluded = false;
 						foreach(Circle p in primitives)
-						{
-							if(Math.Pow(ray.O.X - p.x, 2) + Math.Pow(ray.O.Y - p.y, 2) > p.r * p.r)
+						{						
+							if ((Math.Pow(ray.O.X - p.x, 2) + Math.Pow(ray.O.Y - p.y, 2)) > (p.r * p.r))
 							{
-								if(ray.intersection2(p) && !p.Equals(c)) occluded = true;
-								
-							}else
-							{
-								if(p.Equals(c)) occluded = true; pixelColour += c.colour;
-								if(!p.Equals(c))pixelColour += p.colour; occluded = true;
-								
+								if (ray.intersection(p)) 
+								{ 
+									occluded = true;
+									float tmp = (float)Math.Sqrt((Math.Pow(c.x - p.x, 2) + Math.Pow(c.y - p.y, 2)));
+									if (ray.t < tmp && (ray.t > 0 && tmp > 0)) occluded = false;
+									tmp = (float)Math.Sqrt((Math.Pow(ray.O.X - p.x, 2) + Math.Pow(ray.O.Y - p.y, 2)));
+									if (ray.t < tmp) occluded = false;
+								}
 							}
-							
+							else
+							{
+								occluded = true;
+							}
 						}
-						if(!occluded) 
+						if (!occluded) 
 						{
-							pixelColour += c.colour * lightAttenuation(Vector2.Distance(ray.O, new Vector2(c.x, c.y)));
+							pixelColour += c.colour * lightAttenuation(Vector2.Distance(ray.O, new Vector2(c.x, c.y)), c.r);
 						}
 						screen.Plot(x, y, pixelColour.ToRGB32());
 					}
 				}
 			}
+
 		}
 		// tick: renders one frame
 		public void Tick()
 		{
 
 		}
-
 		Vector2 pixelPosition(int x, int y)
 		{
-			float TX =  ((float)x/640) * 2 - 1;
-			float TY = ((float)y/400) * -2 + 1;
+			float TX =  ((float)x/640.0f) * 2.0f - 1.0f;
+			float TY = ((float)y/400.0f) * -2.0f + 1.0f;
 			return new Vector2(TX, TY);
 		}
 
-		floatColour lightAttenuation(float dl) 
+		floatColour lightAttenuation(float dl, float L) 
 		{
-			float i =  Clamp(0.05f/dl, 0f, 1f);
+			float i =  Clamp((L/2)/dl, 0f, 1f);
 			return new floatColour(i, i, i);
 		}
 		public static float Clamp(float value, float min, float max)  
@@ -86,6 +94,8 @@ namespace Template
 			}
 		
 	}
+
+
 
 	public class floatColour
 	{
@@ -107,6 +117,9 @@ namespace Template
 
 		public int MixColor( int red, int green, int blue )
 		{
+			red = Math.Min(red, 255);
+			green = Math.Min(green, 255);
+			blue = Math.Min(blue, 255);
 			return (red << 16) + (green << 8) + blue;
 		}
 
@@ -173,7 +186,7 @@ namespace Template
 		{
 			float a = Vector2.Dot(D, D);
 			float b = Vector2.Dot(2 * D, O - new Vector2(p.x, p.y));
-			float c = Vector2.Dot(O - new Vector2(p.x, p.y), new Vector2(p.x, p.y)) - (p.r * p.r);
+			float c = Vector2.Dot(O - new Vector2(p.x, p.y), (O - new Vector2(p.x, p.y))) - (p.r * p.r);
 			if((b * b - 4 * a * c) < 0)return false;
 			return true;
 		}
